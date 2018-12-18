b1b9e1fa5c31d227bdb063ed9c72721b3968b63a
Eugene Arbatsky <eugine@elab.pro>
2018-12-18 16:17:50 +0800

    NodeJS project v.0.2 (GET is ready, POST JSON ready)

--- js/app/routes/note_routes.js
+++ js/app/routes/note_routes.js
@@ -1,13 +1,94 @@
+const imagesDir = "images.full";
+const imagesThumbDir = "images.thumb";
+
+const fs = require('fs');
+const http = require('http');
+
+function getCounter() {
+    return 2;
+}
+
+function getFilename(id) {
+    return imagesDir + "/" + id;
+}
+
 module.exports = function (app, db) {
+    /**
+     * 
+     */
     app.post('/api/images', (req, res) => {
-        res.send('Hello POST')
+        if (req.header("Content-Type") == "application/json") {
+            console.log("JSON");
+            var jsonString = '';
+
+            req.on('data', function (data) {
+                jsonString += data;
+            });
+
+            req.on('end', function () {
+                res.contentType('application/json');
+                filesId = [];
+                records = JSON.parse(jsonString);
+                records.forEach(record => {
+                    if (record.url != null) {
+                        fileId = getCounter();
+                        filename = getFilename(fileId);
+                        console.log("Downloading " + record.url + " to file " + filename);
+                        var file = fs.createWriteStream(filename);
+                        var request = http.get(record.url, function (response) {
+                            response.pipe(file);
+                        });
+                        filesId.push(fileId);
+                    }
+                    if (record.content != null) {
+                        fileId = getCounter();
+                        filename = getFilename(fileId);
+                        console.log("Saving content to " + filename);
+                        require("fs").writeFile(filename, record.content, 'base64', function (err) {
+                            console.log(err);
+                        });
+                        filesId.push(fileId);
+                    }
+                });
+                res.send(JSON.stringify(filesId));
+            });
+
+        } else {
+            console.log("MP");
+            res.send('Hello POST')
+        }
     });
+    /**
+     * 
+     */
     app.get('/api/images', (req, res) => {
-        res.send('Hello GET')
+        fs.readdir(imagesDir, (err, files) => {
+            filesId = [];
+            files.forEach(file => {
+                filesId.push(file);
+            });
+            res.contentType('application/json');
+            res.send(JSON.stringify(filesId));
+        })
+
     });
+    /**
+     * 
+     */
     app.get('/api/images/:id', (req, res) => {
         const id = req.params.id;
-        res.send('Hello GET ' + id)
-
+        filename = getFilename(id);
+        fs.exists(filename, function (exists) {
+            if (exists) {
+                res.writeHead(200, {
+                    "Content-Type": "image/png",
+                    "Content-Disposition": "attachment; filename=" + id + ".png"
+                });
+                fs.createReadStream(filename).pipe(res);
+            } else {
+                res.writeHead(400, {"Content-Type": "text/plain"});
+                res.end("ERROR File does not exist");
+            }
+        });
     });
 };
--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
 
-const port = 8080;
+const port = 8000;
 require('./app/routes')(app, {});
 app.listen(port, () => {
-  console.log('We are live on ' + port);
+  console.log('Main API on ' + port);
 });
 
+const portForTests = 8080;
+app.listen(portForTests, () => {
+  console.log('Secondary API on ' + portForTests);
+});
