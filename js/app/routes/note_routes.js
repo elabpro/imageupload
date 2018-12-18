156a967093affc5348a6282c44667c3e2b5ab4c2
Eugene Arbatsky <eugine@elab.pro>
2018-12-18 21:52:04 +0800

    + comments refactoring

--- js/app/routes/note_routes.js
+++ js/app/routes/note_routes.js
@@ -12,9 +12,60 @@ function getFilename(id) {
     return imagesDir + "/" + id;
 }
 
+/**
+ * Create file from base64 encoded content
+ *
+ * @param string content
+ * @returns int
+ */
+function createImageFromContent(content) {
+    var fileId = getCounter();
+    filename = getFilename(fileId);
+    console.log("Saving content to " + filename);
+    require("fs").writeFile(filename, content, 'base64', function (err) {
+        console.log(err);
+    });
+    return fileId;
+}
+
+/**
+ * Create file from uploaded file
+ * @param tmpFilename
+ * @return
+ */
+function createImageFromFile() {
+
+}
+
+/**
+ * Download and save image to a file
+ *
+ * @param string url
+ * @returns int
+ */
+function downloadFile(url) {
+    var fileId = getCounter();
+    filename = getFilename(fileId);
+    console.log("Downloading " + url + " to file " + filename);
+    var file = fs.createWriteStream(filename);
+    var request = http.get(record.url, function (response) {
+        response.pipe(file);
+    });
+    return fileId;
+}
+
 module.exports = function (app, db) {
     /**
-     * 
+     * Method POST
+     * Upload images in PNG. Body:
+     * - multipart/encoded files
+     * - JSON {{content=#base64_image_content#|url=#url#}}
+     * - url=#url#
+     *
+     * @usage http://localhost/api/images
+     *
+     * @param req
+     * @param res
      */
     app.post('/api/images', (req, res) => {
         if (req.header("Content-Type") == "application/json") {
@@ -31,22 +82,11 @@ module.exports = function (app, db) {
                 records = JSON.parse(jsonString);
                 records.forEach(record => {
                     if (record.url != null) {
-                        fileId = getCounter();
-                        filename = getFilename(fileId);
-                        console.log("Downloading " + record.url + " to file " + filename);
-                        var file = fs.createWriteStream(filename);
-                        var request = http.get(record.url, function (response) {
-                            response.pipe(file);
-                        });
+                        fileId = downloadFile(record.url);
                         filesId.push(fileId);
                     }
                     if (record.content != null) {
-                        fileId = getCounter();
-                        filename = getFilename(fileId);
-                        console.log("Saving content to " + filename);
-                        require("fs").writeFile(filename, record.content, 'base64', function (err) {
-                            console.log(err);
-                        });
+                        fileId = createImageFromContent(record.content);
                         filesId.push(fileId);
                     }
                 });
@@ -58,8 +98,15 @@ module.exports = function (app, db) {
             res.send('Hello POST')
         }
     });
+
     /**
-     * 
+     * Method GET Getting array of images (JSON)
+     * On return sends JSON array of filenames
+     *
+     * @usage http://localhost:8080/api/images
+     *
+     * @param req
+     * @param res
      */
     app.get('/api/images', (req, res) => {
         fs.readdir(imagesDir, (err, files) => {
@@ -72,8 +119,15 @@ module.exports = function (app, db) {
         })
 
     });
+
     /**
-     * 
+     * Method GET Getting an image
+     * On return sends binary image
+     *
+     * @usage http://localhost:8080/api/images/{id}
+     *
+     * @param req
+     * @param res
      */
     app.get('/api/images/:id', (req, res) => {
         const id = req.params.id;
--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const app            = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+console.log("NodeJS project v.0.3. Image API is ready");
+console.log("URL http://localhost:8080/api/images");
+console.log("Source code from https://github.com/elabpro/imageupload");
 
 const port = 8000;
 require('./app/routes')(app, {});
 app.listen(port, () => {
-  console.log('Main API on ' + port);
+    console.log('Main API on ' + port);
 });
 
 const portForTests = 8080;
 app.listen(portForTests, () => {
-  console.log('Secondary API on ' + portForTests);
+    console.log('Secondary API on ' + portForTests);
 });
