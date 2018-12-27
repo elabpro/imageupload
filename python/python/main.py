98e5ad193e8d6653c5ecab3e01b6fed5b399a00c
Eugene Arbatsky <eugine@elab.pro>
2018-12-27 08:23:31 +0800

    +update documentation

--- python/python/main.py
+++ python/python/main.py
@@ -1,27 +1,36 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+# ImageUpload API on Python
+# 2018 (c) Eugene Arbatsky
 
-from flask import Flask, jsonify, send_file, Response, request
-import os
+from flask import Flask
+from flask import Response
+from flask import jsonify
+from flask import request
+from flask import send_file
 import json
+import os
 import redis
 import urllib
 
 app = Flask(__name__)
 
 imagesDir = "images.full"
-imagesThumbDir = "images.	thumb"
+imagesThumbDir = "images.thumb"
 
+# GET
+# /api/images
 @app.route('/api/images', methods=['GET'])
 def getImages():
     files = os.listdir(imagesDir)
     return jsonify(files)
 
+# GET
+# /api/images/{id}
 @app.route('/api/images/<int:imageId>', methods=['GET'])
 def getImage(imageId):
-    return send_file(imagesDir + "/" + str(imageId),  mimetype='image/png')
+    return send_file(imagesDir + "/" + str(imageId), mimetype='image/png')
 
+# POST
+# /api/images
 @app.route('/api/images', methods=['POST'])
 def postImage():
     files = []
@@ -41,27 +50,35 @@ def postImage():
         try:
             url = request.form.get("url")
             if url is not None and len(url) > 0:
-               files.append(download(url))
+                files.append(download(url))
         except KeyError:
             print "No URL"
         for fileInUpload in request.files:
             file = request.files[fileInUpload]
             fileId = getCounter()
             filename = getFilename(fileId)
-            print "Saving file "+file.filename+" to " + filename
+            print "Saving file " + file.filename + " to " + filename
             file.save(filename)
             files.append(fileId)
     return json.dumps(files)
 
+# Download from from URL
+#
+# @param string url
+# @return int fileId
 def download(url):
     fileId = getCounter()
     filename = getFilename(fileId)
     print "Downloading " + url + " to " + filename
     createLock(fileId)
-    urllib.urlretrieve(url,filename)
+    urllib.urlretrieve(url, filename)
     deleteLock(fileId)
     return fileId
 
+# Upload file from BASE64 content
+#
+# @param string
+# @return int fileId
 def uploadContent(content):
     fileId = getCounter()
     filename = getFilename(fileId)
@@ -73,18 +90,29 @@ def uploadContent(content):
     deleteLock(fileId)
     return fileId
 
+# Get ID from redis
+# @return int fileId
 def getCounter():
     r = redis.Redis(host='localhost', port=6379, db=0)
     return r.incr("counter")
 
+# Get filename for an image with specified ID
+#
+# @param int fileId
 def getFilename(fileId):
     return "images.full/" + str(fileId)
 
+# Create lock for an image with specified ID
+#
+# @param int fileId
 def createLock(fileId):
-    fh = open(getFilename(fileId)+".lock", "wb")
+    fh = open(getFilename(fileId) + ".lock", "wb")
     fh.write("1")
     fh.close()
 
+# Delete lock for an image with specified ID
+#
+# @param int fileId
 def deleteLock(fileId):
     os.remove(getFilename(fileId) + ".lock")
 
